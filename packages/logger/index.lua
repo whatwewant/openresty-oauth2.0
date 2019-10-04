2a9081043ae367fce1670f656abe8bb8d134430d
whatwewant <tobewhatwewant@gmail.com>
2019-10-04 10:40:53 +0800

    :recycle:(logger): refactor logger with logger + context + request_context for debug

--- packages/logger/index.lua
+++ packages/logger/index.lua
@@ -1,24 +1,61 @@
 local cjson = require('cjson')
-local core_config = require('oauth/core/config')
+local config = require('oauth/core/config')
 local object = require('oauth/utils/object')
 
 local stringify = cjson.encode
-local merge = object.merge
+-- local merge = object.merge
 
-local debug_mode = core_config.debug
+local debug_mode = config.debug
 
 local _M = {}
 
+local context = nil
+
+-- Static Context, such as config
+local function getContext()
+  if not context then
+    context = {
+      config = config,
+    }
+  end
+  
+  return context
+end
+
+-- Dynamic Rqeuest Context, such as request info
+local function get_request_context()
+  return {
+    uri = ngx.var.uri,
+    method = ngx.req.get_method(),
+    headers = ngx.req.get_headers(),
+    query = ngx.req.get_uri_args(),
+    ip = ngx.var.remote_addr,
+    http_version = ngx.req.http_version(),
+    start_time = ngx.req.start_time(),
+    response_time = ngx.now() - ngx.req.start_time(),
+    created_at = ngx.now(),
+  }
+end
+
+local function get_body(json)
+  return stringify({
+    logger = json,
+    context = getContext(),
+    requestContext = get_request_context(),
+  })
+end
+
 local function info(json)
-  local body = stringify(merge(json, { debug = debug_mode }))
+  local body = get_body(json)
   ngx.info(ngx.ERR, body)
 end
 
 local function debug(json, status)
-  local body = stringify(merge(json, { debug = debug_mode }))
+  local body = get_body(json)
 
   if debug_mode then
     ngx.say(body)
+    ngx.header['Content-Type'] = 'application/json; charset=utf-8'
     return ngx.exit(status or ngx.HTTP_INTERNAL_SERVER_ERROR)
   end
 
