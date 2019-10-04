69cf8d8e65c6570a4fb3318785bb1f8d5d38ec58
whatwewant <tobewhatwewant@gmail.com>
2019-10-04 10:02:15 +0800

    :sparkles:(logger.debug): debug method support json and status args, status only works in debug mode

--- packages/logger/index.lua
+++ packages/logger/index.lua
@@ -1,23 +1,25 @@
 local cjson = require('cjson')
 local core_config = require('oauth/core/config')
+local object = require('oauth/utils/object')
 
 local stringify = cjson.encode
+local merge = object.merge
 
 local debug_mode = core_config.debug
 
 local _M = {}
 
 local function info(json)
-  local body = stringify(json)
+  local body = stringify(merge(json, { debug = debug_mode }))
   ngx.info(ngx.ERR, body)
 end
 
-local function debug(json)
-  local body = stringify(json)
+local function debug(json, status)
+  local body = stringify(merge(json, { debug = debug_mode }))
 
   if debug_mode then
     ngx.say(body)
-    return ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
+    return ngx.exit(status or ngx.HTTP_INTERNAL_SERVER_ERROR)
   end
 
   ngx.log(ngx.ERR, body)
