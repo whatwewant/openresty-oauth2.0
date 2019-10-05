cd6d7cafc026ad014ce45b5f52348eff4684873a
whatwewant <tobewhatwewant@gmail.com>
2019-10-05 15:58:52 +0800

    :bookmarl:(core): bumped version to v1.0.4

--- packages/core/config.lua
+++ packages/core/config.lua
@@ -4,7 +4,7 @@ local object = require('oauth/utils/object')
 local split = object.split
 local merge = object.merge
 
-local version = '1.0.3'
+local version = '1.0.4'
 
 local PRODUCTION = 'production'
 local ALLOW_ALL = 'all'
