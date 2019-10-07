f900f10a72aa6abd9f33d45da895667ef5a7c2d8
whatwewant <tobewhatwewant@gmail.com>
2019-10-07 18:11:59 +0800

    :lisptic:(core.config): update config file

--- packages/core/config.lua
+++ packages/core/config.lua
@@ -15,6 +15,12 @@ local provider = getenv('PROVIDER')
 local allow_usernames_str = getenv('ALLOW_USERNAMES') or 'all'
 
 local redirect_uri = root_url..'/_oauth/'..provider
+local response_type = getenv('RESPONSE_TYPE') or 'code'
+local scope = getenv('SCOPE') or ''
+
+local authorize_url = getenv('AUTHORIZE_URL')
+local token_url = getenv('TOKEN_URL')
+local user_info_url = getenv('USER_INFO_URL')
 
 local client_id = getenv('CLIENT_ID')
 local client_secret = getenv('CLIENT_SECRET')
@@ -44,14 +50,14 @@ local config = {
   -- @2 URL INFO
   -- @2.1 GET AUTHORIZE_URL
   --   args: ?client_id=CLIENT_ID&redirect_uri=REDIRECT_URI&scope=SCOPE&state=xxx
-  authorize_url = getenv('AUTHORIZE_URL'),
+  authorize_url = authorize_url,
   -- using to authorize, default: code
-  response_type = getenv('RESPONSE_TYPE') or 'code',
-  scope = getenv('SCOPE'),
+  response_type = response_type,
+  scope = scope,
 
   -- @2.2 POST TOKEN_URL
   --  body: { client_id, client_secret, redirect_uri, code, state }
-  token_url = getenv('TOKEN_URL'),
+  token_url = token_url,
   client_secret = client_secret,
   -- using to get access token, default: authorization_code
   grant_type = getenv('GRANT_TYPE') or 'authorization_code',
@@ -62,7 +68,7 @@ local config = {
 
   -- @2.3 GET USER_INFO_URL
   --  header: { Authorization: Bearer Token; }
-  user_info_url = getenv('USER_INFO_URL'),
+  user_info_url = user_info_url,
 
   -- @2.3.1 Extendable Token in Header, Query, Body
   user_info_header = getenv('USER_INFO_HEADER'),
