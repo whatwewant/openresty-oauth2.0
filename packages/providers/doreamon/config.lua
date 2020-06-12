f9817ad16c8eb1c2cb798f53b9e091727796743b
whatwewant <tobewhatwewant@gmail.com>
2020-06-12 11:18:33 +0800

    feat(providers): upgrade doreamon provider

--- packages/providers/doreamon/config.lua
+++ packages/providers/doreamon/config.lua
@@ -12,7 +12,7 @@ local config = {
   -- @2 URL INFO
   -- @2.1 GET AUTHORIZE_URL
   --   args: ?client_id=CLIENT_ID&redirect_uri=REDIRECT_URI&scope=SCOPE&state=xxx
-  authorize_url = 'https://id.zcorky.com/oauth/authorize',
+  authorize_url = 'https://login.zcorky.com/',
   -- using to authorize, default: code
   response_type = 'code',
   scope = '_', -- @TODO
@@ -20,7 +20,7 @@ local config = {
 
   -- @2.2 POST TOKEN_URL
   --  body: { client_id, client_secret, redirect_uri, code, state }
-  token_url = 'https://id.zcorky.com/oauth/token',
+  token_url = 'https://login.zcorky.com/token',
   -- using to get access token, default: authorization_code
   grant_type = 'authorization_code',
 
@@ -30,7 +30,7 @@ local config = {
 
   -- @2.3 GET USER_INFO_URL
   --  header: { Authorization: Bearer Token; }
-  user_info_url = 'https://id.zcorky.com/oauth/user',
+  user_info_url = 'https://login.zcorky.com/user',
 
   -- @2.3.1 Extendable Token in Header, Query, Body
   user_info_header = 'Authorization',
